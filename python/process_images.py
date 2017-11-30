7223573b07b373ff3dc9162f93c4dccda6b64e16
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2017-11-30 18:10:59 -0500

    Create an error table in the database

--- python/process_images.py
+++ python/process_images.py
@@ -19,6 +19,11 @@ def parse_command_line():
                             use standard file glob wildcards like '*', '?',
                             or '**'. Note: you may need to quote this argument
                             like so: --files 'path/to/images/*.jpg'""")
+
+    parser.add_argument('-c', '--create-table', action='store_true',
+                        help="""Drop and recreate the images table in the
+                            SQL database.""")
+
     args = parser.parse_args()
     return args
 
@@ -29,38 +34,60 @@ def get_image_data(file_name):
         exif = exifread.process_file(image_file)
         image = Image.open(image_file)
         image.load()
+
     qr_code = zbarlight.scan_codes('qrcode', image)
-    return str(exif['Image DateTime']), qr_code[0].decode('utf-8')
+    qr_code = qr_code[0].decode('utf-8') if qr_code else None
+
+    return str(exif['Image DateTime']), qr_code
+
+
+def create_tables(db_conn):
+    """Reset the images table."""
+    db_conn.execute('DROP TABLE IF EXISTS images')
+    db_conn.execute("""
+                    CREATE TABLE images (
+                      id            TEXT PRIMARY KEY,
+                      file_name     TEXT UNIQUE,
+                      image_created TEXT)
+                     """)
+    db_conn.execute('DROP TABLE IF EXISTS errors')
+    db_conn.execute("""CREATE TABLE errors (msg TEXT)""")
 
 
 def main():
     """Run the program loop."""
     uuids = {}
-    duplicates = 0
+
     args = parse_command_line()
 
     db_name = join('data', 'nitfix.sqlite.db')
-    sql = """
+    insert = """
         INSERT INTO images (id, file_name, image_created) VALUES (?, ?, ?)
         """
+    error = """INSERT INTO errors (msg) VALUES (?)"""
 
     with sqlite3.connect(db_name) as db_conn:
+        if args.create_table:
+            create_tables(db_conn)
+
         for pattern in args.files:
             for file_name in sorted(glob(pattern)):
                 image_created, uuid = get_image_data(file_name)
                 print(file_name, uuid)
 
-                # Skip duplicate images
-                if uuids.get(uuid):
-                    duplicates += 1
-                    print('DUPLICATE')
-                    continue
-                uuids[uuid] = 1
-
-                db_conn.execute(sql, (uuid, file_name, image_created))
+                if not uuid:
+                    msg = 'MISSING: QR code missing in {}'.format(file_name)
+                    print(msg)
+                    db_conn.execute(error, (msg, ))
+                elif uuids.get(uuid):
+                    msg = ('DUPLICATES: Files {} and {} have the same '
+                           'QR code').format(uuids[uuid], file_name)
+                    print(msg)
+                    db_conn.execute(error, (msg, ))
+                else:
+                    uuids[uuid] = file_name
+                    db_conn.execute(insert, (uuid, file_name, image_created))
                 db_conn.commit()
-
-        print('Duplicates', duplicates)
 
 
 if __name__ == '__main__':
