143fd3f8b576be0925c6f7f925e07a64e707d62e
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2017-12-15 13:51:09 -0500

    Add dropbox API and code cleanup

--- python/nfn_expedition.py
+++ python/nfn_expedition.py
@@ -1,12 +1,14 @@
 """Build a Notes from Nature expedition."""
 
-# import os
-import csv
+import os
 from os.path import join, split
+import csv
+from shutil import copyfile
 import lib.db as db
 
 
-NYBG_DIR = join('data', 'nybg_expedition')
+EXPEDITION_DIR = join('data', 'nybg_expedition')
+PROVIDER = 'NYBG'
 
 
 def get_absent_data(db_conn):
@@ -22,43 +24,48 @@ def get_absent_data(db_conn):
                 record = get_image_info(db_conn, row, no_image)
                 if record:
                     absent.append(record)
-                    print(record)
     return absent, no_image
 
 
 def get_image_info(db_conn, row, no_image):
     """Get the image info from the taxonomies and images tables."""
-    taxonomy = db.get_taxonomy_by_provider(db_conn, 'NYBG', row[1])
+    taxonomy = db.get_taxonomy_by_provider(db_conn, PROVIDER, row[1])
     image = db.get_image(db_conn, taxonomy[5])
     if not image:
-        no_image.append(('NYBG {}'.format(row[1]), taxonomy[2]))
+        no_image.append(('{} {}'.format(PROVIDER, row[1]), taxonomy[2]))
         return None
-    # _, file_name = split(image[1])
-    return (image[1], 'NYBG {}'.format(row[1]), image[0], taxonomy[2])
+    return (image[1], '{} {}'.format(PROVIDER, row[1]), image[0], taxonomy[2])
 
 
 def copy_images(absent):
     """Copy images to the expedition repository."""
-    print(absent)
+    for image in absent:
+        _, file_name = split(image[0])
+        path = join(EXPEDITION_DIR, file_name)
+        copyfile(image[0], path)
 
 
 def create_manifest(absent):
     """Create expedition manifest."""
-    print(absent)
+    csv_path = join(EXPEDITION_DIR, 'nybg_expedition.csv')
+    with open(csv_path, 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(
+            ['image_name', 'nybg_bar_code', 'qr_code', 'resolved_name'])
+        for image in absent:
+            _, file_name = split(image[0])
+            writer.writerow([file_name, image[1], image[2], image[3]])
 
 
 def main():
     """Create expedition."""
     with db.connect() as db_conn:
         absent, no_image = get_absent_data(db_conn)
-    print(len(absent))
+    os.makedirs(EXPEDITION_DIR, exist_ok=True)
+    copy_images(absent)
+    create_manifest(absent)
     for problem in no_image:
         print(problem)
-    print(len(no_image))
-
-    # os.makedirs(NYBG_DIR, exist_ok=True)
-    # copy_images(absent)
-    # create_manifest(absent)
 
 
 if __name__ == '__main__':
