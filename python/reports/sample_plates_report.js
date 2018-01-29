de4b5c7f05d6156d614e44fc867179a74847d396
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2018-01-29 11:18:35 -0500

    Update sample plates report

--- python/reports/sample_plates_report.js
+++ python/reports/sample_plates_report.js
@@ -1,18 +1,12 @@
-// Toggle a group of rows open/closed. That is we will show/hide all records
-// for a group. Closing them leaves only the first record (the reconciled one)
-// visible.
+// Toggle a group of rows open/closed. That is we will show/hide all genus records
+// for a family. Closing them leaves only the first record (the family one) visible.
 
-const tbodyFamilies = document.querySelector('.families tbody');
-
-function toggleFamilyClosed(event) {
-  if (! event.target.matches('button')) { return; }
-  const family = event.target.dataset.family;
-  console.log(family)
-  const selector = '.families tr[data-family="' + family + '"]';
-  const trs = document.querySelectorAll(selector);
-  trs.forEach(function(r) { r.classList.toggle('closed'); });
-  // const isClosed = event.target.parentElement.parentElement.classList.contains('closed');
-  // groupState[groupBy] = isClosed ? 'closed' : '';
-}
-
-tbodyFamilies.addEventListener('click', toggleFamilyClosed);
+document.querySelector('.families tbody')
+  .addEventListener('click', function (event) {
+    if (! event.target.matches('button')) { return; }
+    const family = event.target.dataset.family;
+    console.log(family)
+    const selector = '.families tr[data-family="' + family + '"]';
+    const trs = document.querySelectorAll(selector);
+    trs.forEach(function(r) { r.classList.toggle('closed'); });
+});
