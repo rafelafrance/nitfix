8012f42fc4240572cb5c5da15f1bd0dcc4ea30ad
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2018-02-02 16:24:44 -0500

    Filter sample plates

--- python/reports/sample_plates_report.js
+++ python/reports/sample_plates_report.js
@@ -1,19 +1,21 @@
 ////////////////////////////////////////////////////////////////////////////////////
-// section
+// Open close report sections
+
 document.querySelectorAll('h2').forEach(function(header) {
   header.addEventListener('click', function(event) {
-    event.target.classList.toggle('closed');
-    const selector = '.' + event.target.dataset.selector;
+    const button = event.target.matches('button') ? event.target : event.target.querySelector('button');
+    button.classList.toggle('closed');
+    const selector = '.' + button.dataset.selector;
     document.querySelector(selector).classList.toggle('closed');
   });
 });
 
 ////////////////////////////////////////////////////////////////////////////////////
-// section.families
+// Toggle a group of Family Coverage rows open/closed. That is we will show/hide
+// all genus records for a family. Closing them leaves only the first record
+// (the family one) visible.
 
-// Toggle a group of rows open/closed. That is we will show/hide all genus records
-// for a family. Closing them leaves only the first record (the family one) visible.
-document.querySelector('.families tbody')
+document.querySelector('section.families tbody')
   .addEventListener('click', function(event) {
     if (! event.target.matches('button')) { return; }
     event.target.classList.toggle('closed');
@@ -25,24 +27,132 @@ document.querySelector('.families tbody')
 
 
 ////////////////////////////////////////////////////////////////////////////////////
-// section.plates
+// Data passed in from python for the Sample Plates report section
 
-const plates = {{ plates | safe }};
-const wells = {{ wells | safe }};
-var maxPage = plates.length;
+const allPlates = {{ plates | safe }};
+const allWells = {{ wells | safe }};
 
-function filterChange() {
-  // maxPage = Math.ceil(filters[filter].length / args.page_size);
-  document.querySelector('.pager').setAttribute('max', maxPage);
-  document.querySelector('.max-page').innerHTML = 'of ' + maxPage;
+////////////////////////////////////////////////////////////////////////////////////
+// Filter logic for the Sample Plates report section
+
+let filteredPlates = allPlates.filter(function(plate) { return true; });
+
+function debounce(func, wait, immediate) {
+  // Taken from underscore.js
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+
+function filterWells(plate) {
+  const scientific_name =( document.querySelector('.search-control [data-field=scientific_name]').value || '').toLowerCase();
+  const sample_id = (document.querySelector('.search-control [data-field=sample_id]').value || '').toLowerCase();
+  return allWells[plate.plate_id].filter(function (well) {
+    if (!scientific_name && !sample_id) {
+      return true;
+    }
+    if (!scientific_name) {
+      return well.sample_id.toLowerCase().indexOf(sample_id) > -1;
+    }
+    if (!sample_id) {
+      return well.scientific_name.toLowerCase().indexOf(scientific_name) > -1;
+    }
+    return well.scientific_name.toLowerCase().indexOf(scientific_name) > -1
+        && well.sample_id.toLowerCase().indexOf(sample_id) > -1;
+  });
+}
+
+function filterChange(target) {
+  filteredPlates = allPlates.filter(function(plate) {
+    const filtered = filterWells(plate);
+    return filtered.length;
+  });
+  resetPager();
+}
+
+
+document.querySelectorAll('section.samples .search-control input')
+  .forEach(function(input) {
+    input.value = '';
+    filterChange(input);
+    input.addEventListener('keyup', debounce(function(event) {
+      filterChange(event.target);
+    }, 250));
+});
+
+
+document.querySelectorAll('section.samples .search-control button')
+  .forEach(function(btn) {
+    btn.addEventListener('click', debounce(function(event) {
+      const input = btn.previousElementSibling;
+      input.value = '';
+      filterChange(input);
+    }));
+});
+
+
+////////////////////////////////////////////////////////////////////////////////////
+// Pager logic for the Sample Plates report section
+
+var maxPage;
+
+function changePage() {
+  const pager = document.querySelector('.pager');
+  var page = +pager.value;
+  page = page < 1 ? 1 : page;
+  page = page > maxPage ? maxPage : page;
+  pager.value = page;
+  const rows = buildTableRows(page);
+  buildTable(rows);
+}
+
+document.querySelector('.first-page').addEventListener('click', function() {
+  const pager = document.querySelector('.pager');
+  pager.value = 1;
+  changePage();
+});
+
+document.querySelector('.previous-page').addEventListener('click', function() {
+  const pager = document.querySelector('.pager');
+  pager.value = +pager.value - 1;
+  changePage();
+});
+
+document.querySelector('.next-page').addEventListener('click', function() {
+  const pager = document.querySelector('.pager');
+  pager.value = +pager.value + 1;
+  changePage();
+});
+
+document.querySelector('.last-page').addEventListener('click', function() {
+  const pager = document.querySelector('.pager');
+  pager.value = maxPage;
+  changePage();
+});
+
+document.querySelector('.pager').addEventListener('change', changePage);
+
+function resetPager() {
+  maxPage = filteredPlates.length;
+  document.querySelector('section.samples .max-page').innerHTML = 'of ' + maxPage;
+  document.querySelector('section.samples .pager').value = '1';
   changePage();
 }
 
-document.querySelectorAll('.search').forEach(function(search) {
-  search.addEventListener('keyup', function(event) {
-    console.log(event.target.value);
-  });
-});
+resetPager();
+
+////////////////////////////////////////////////////////////////////////////////////
+// Logic for build the Sample Plates table
 
 function buildTable(rows) {
   const tbody = document.querySelector('section.samples tbody');
@@ -125,50 +235,14 @@ function buildWellData(well) {
 
 function buildTableRows(page) {
   const rows = [];
-  const plate = plates[page - 1];
-  rows.push(buildPlateHeader());
-  rows.push(buildPlateData(plate));
-  rows.push(buildWellHeader());
-  wells[plate.plate_id].forEach(function(well) {
-    rows.push(buildWellData(well));
-  });
+  if (page) {
+    const plate = filteredPlates[page - 1];
+    rows.push(buildPlateHeader());
+    rows.push(buildPlateData(plate));
+    rows.push(buildWellHeader());
+    filterWells(plate).forEach(function(well) {
+      rows.push(buildWellData(well));
+    });
+  }
   return rows;
 }
-
-function changePage() {
-  const pager = document.querySelector('.pager');
-  var page = +pager.value;
-  page = page < 1 ? 1 : page;
-  page = page > maxPage ? maxPage : page;
-  pager.value = page;
-  const rows = buildTableRows(page);
-  buildTable(rows);
-}
-
-document.querySelector('.first-page').addEventListener('click', function() {
-  const pager = document.querySelector('.pager');
-  pager.value = 1;
-  changePage();
-});
-
-document.querySelector('.previous-page').addEventListener('click', function() {
-  const pager = document.querySelector('.pager');
-  pager.value = +pager.value - 1;
-  changePage();
-});
-
-document.querySelector('.next-page').addEventListener('click', function() {
-  const pager = document.querySelector('.pager');
-  pager.value = +pager.value + 1;
-  changePage();
-});
-
-document.querySelector('.last-page').addEventListener('click', function() {
-  const pager = document.querySelector('.pager');
-  pager.value = maxPage;
-  changePage();
-});
-
-document.querySelector('.pager').addEventListener('change', changePage);
-
-changePage();
