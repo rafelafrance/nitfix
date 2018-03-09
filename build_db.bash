cecf811e34131841976a36268630ad67b2d4eb4b
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2018-03-09 12:02:41 -0500

    Update database build script to include MOBOT data

--- build_db.bash
+++ build_db.bash
@@ -4,9 +4,10 @@ set -e
 set -x
 
 python src/process_images.py -f 'data/raw/DOE-nitfix_specimen_photos/*.JPG' -c
-python src/process_images.py -f 'data/raw/raw/HUH_DOE-nitfix_specimen_photos/*.JPG'
+python src/process_images.py -f 'data/raw/HUH_DOE-nitfix_specimen_photos/*.JPG'
 python src/process_images.py -f 'data/raw/OS_DOE-nitfix_specimen_photos/*.JPG'
-python src/process_images.py -f 'data/raw/raw/CAS-DOE-nitfix_specimen_photos/*.JPG'
+python src/process_images.py -f 'data/raw/CAS-DOE-nitfix_specimen_photos/*.JPG'
+python src/process_images.py -f 'data/raw/MO-DOE-nitfix_specimen_photos/*.JPG'
 
 python src/get_master_taxonomy.py
 python src/get_uuids.py
@@ -15,13 +16,13 @@ python src/get_uuids.py
 # python src/process_images.py -f 'data/raw/DOE-nitfix_specimen_photos/R0000674.JPG' --set-uuid 3d83f5f0-419f-4aaf-be4d-2773f2683412
 
 # Note error resolutions
-python src/resolve_error.py -k data/raw/raw/DOE-nitfix_specimen_photos/R0000149.JPG -r 'OK: Genuine duplicate'
+python src/resolve_error.py -k data/raw/DOE-nitfix_specimen_photos/R0000149.JPG -r 'OK: Genuine duplicate'
 python src/resolve_error.py -k data/raw/DOE-nitfix_specimen_photos/R0000151.JPG -r 'OK: Genuine duplicate'
-python src/resolve_error.py -k data/raw/raw/DOE-nitfix_specimen_photos/R0000158.JPG -r 'OK: Genuine duplicate'
+python src/resolve_error.py -k data/raw/DOE-nitfix_specimen_photos/R0000158.JPG -r 'OK: Genuine duplicate'
 python src/resolve_error.py -k data/raw/DOE-nitfix_specimen_photos/R0000165.JPG -r 'OK: Genuine duplicate'
-python src/resolve_error.py -k data/raw/raw/DOE-nitfix_specimen_photos/R0000674.JPG -r 'OK: Is a duplicate of R0000473'
+python src/resolve_error.py -k data/raw/DOE-nitfix_specimen_photos/R0000674.JPG -r 'OK: Is a duplicate of R0000473'
 python src/resolve_error.py -k data/raw/DOE-nitfix_specimen_photos/R0000835.JPG -r 'OK: Is a duplicate of R0000836'
-python src/resolve_error.py -k data/raw/raw/DOE-nitfix_specimen_photos/R0000895.JPG -r 'OK: Genuine duplicate'
+python src/resolve_error.py -k data/raw/DOE-nitfix_specimen_photos/R0000895.JPG -r 'OK: Genuine duplicate'
 python src/resolve_error.py -k data/raw/DOE-nitfix_specimen_photos/R0000937.JPG -r 'OK: Genuine duplicate'
 python src/resolve_error.py -k data/raw/DOE-nitfix_specimen_photos/R0001055.JPG -r 'OK: Genuine duplicate'
 python src/resolve_error.py -k data/raw/HUH_DOE-nitfix_specimen_photos/R0001262.JPG -r 'OK: Is a duplicate of R0001263'
@@ -34,3 +35,4 @@ python src/resolve_error.py -k data/raw/CAS-DOE-nitfix_specimen_photos/R0001361.
 python src/resolve_error.py -k data/raw/CAS-DOE-nitfix_specimen_photos/R0002349.JPG -r 'OK: Genuine duplicate'
 
 python src/get_sample_plates.py
+python src/get_picogreen.py
