b1fe7af384b6d47b2a8101e7a9e5a04a5a45c7cd
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2018-08-23 08:25:32 -0400

    Changes script for ingesting Rapid's plate reformatting data

---
+++ src/ingest_rapid_reformatting_data.py
@@ -0,0 +1,45 @@
+"""Extract, transform, & load RAPiD reformatting data."""
+
+import pandas as pd
+import lib.db as db
+import lib.google as google
+import lib.util as util
+
+
+def ingest_rapid_reformat_data():
+    """Ingest data related to the samples."""
+    cxn = db.connect()
+    reformat_data = get_reformatted_wells()
+    create_rapid_reformat_data_table(cxn, reformat_data)
+
+
+def create_rapid_reformat_data_table(cxn, reformat_data):
+    """Create rapid reformat data table table."""
+    reformat_data.to_sql(
+        'rapid_reformat_data', cxn, if_exists='replace', index=False)
+
+    sql = """CREATE UNIQUE INDEX IF NOT EXISTS
+             rapid_reformat_data_row_sort
+             ON rapid_reformat_data (row_sort)"""
+    cxn.execute(sql)
+
+
+def get_reformatted_wells():
+    """Get replated Rapid rata from Google sheet."""
+    csv_path = util.INTERIM_DATA / 'rapid_reformat_data.csv'
+
+    google.sheet_to_csv('FMN_131001_Reformatting_Template.xlsx', csv_path)
+
+    reformat_data = pd.read_csv(
+        csv_path,
+        header=0,
+        na_filter=False,
+        names=[
+            'row_sort', 'source_plate', 'source_well', 'source_well_no',
+            'dest_plate', 'dest_well', 'dest_well_no', 'volume', 'comments'])
+
+    return reformat_data.loc[reformat_data.source_plate != '', :]
+
+
+if __name__ == '__main__':
+    ingest_rapid_reformat_data()
