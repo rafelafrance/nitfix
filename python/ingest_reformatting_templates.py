f6283c75e5cfc479cb9443208e3346fae6817119
rafe <raphael.lafrance@gmail.com>
2019-04-24 09:12:54 -0400

    Ingest all reformatting templates

--- python/ingest_reformatting_templates.py
+++ python/ingest_reformatting_templates.py
@@ -1,37 +1,33 @@
-"""Extract, transform, & load Rapid reformatting data."""
+"""Extract, transform, & load Rapid reformatting templates."""
 
+from os.path import splitext
 import pandas as pd
 import lib.db as db
 import lib.google as google
 import lib.util as util
 
 
-def ingest_rapid_reformat_data():
-    """Ingest data related to the samples."""
+GOOGLE_SHEETS = [
+    'FMN_131001_Reformatting_Template.xlsx',
+    'KIB_135802_Reformatting_template']
+
+
+def ingest_reformating_template(google_sheet):
+    """Ingest one reformatting template."""
     cxn = db.connect()
-    reformat_data = get_reformatted_wells()
-    create_rapid_reformat_data_table(cxn, reformat_data)
+    table, _ = splitext(google_sheet)
+    reformat_wells = get_reformatted_wells(google_sheet)
+    reformat_wells.to_sql(table, cxn, if_exists='replace', index=False)
 
 
-def create_rapid_reformat_data_table(cxn, reformat_data):
-    """Create rapid reformat data table table."""
-    reformat_data.to_sql(
-        'rapid_reformat_data', cxn, if_exists='replace', index=False)
-
-    cxn.execute("""
-        CREATE UNIQUE INDEX IF NOT EXISTS
-            rapid_reformat_data_row_sort
-            ON rapid_reformat_data (row_sort);
-        """)
-
-
-def get_reformatted_wells():
+def get_reformatted_wells(google_sheet):
     """Get replated Rapid rata from Google sheet."""
-    csv_path = util.TEMP_DATA / 'rapid_reformat_data.csv'
+    table, _ = splitext(google_sheet)
+    csv_path = util.TEMP_DATA / f'{table}.csv'
 
-    google.sheet_to_csv('FMN_131001_Reformatting_Template.xlsx', csv_path)
+    google.sheet_to_csv(google_sheet, csv_path)
 
-    reformat_data = pd.read_csv(
+    reformat_wells = pd.read_csv(
         csv_path,
         header=0,
         na_filter=False,
@@ -39,8 +35,27 @@ def get_reformatted_wells():
             'row_sort', 'source_plate', 'source_well', 'source_well_no',
             'dest_plate', 'dest_well', 'dest_well_no', 'volume', 'comments'])
 
-    return reformat_data.loc[reformat_data.source_plate != '', :]
+    return reformat_wells.loc[reformat_wells.source_plate != '', :].copy()
+
+
+def merge_reformating_templates():
+    """Create rapid reformat data table table."""
+    cxn = db.connect()
+
+    merged = None
+    for sheet in GOOGLE_SHEETS:
+        table, _ = splitext(sheet)
+        sheet = pd.read_sql(f'SELECT * from {table};', cxn)
+        if merged is None:
+            merged = sheet
+        else:
+            merged = merged.append(sheet, ignore_index=True)
+
+    merged.to_sql(
+        'reformatting_templates', cxn, if_exists='replace', index=False)
 
 
 if __name__ == '__main__':
-    ingest_rapid_reformat_data()
+    for SHEET in GOOGLE_SHEETS:
+        ingest_reformating_template(SHEET)
+    merge_reformating_templates()
